ba8c9cd9a081c83d617d7788f8a08acf6b723eaf
usausa <machi.pon@gmail.com>
2017-05-12 21:27:30 +0900

    Add projects

---
+++ Smart.Reflection.Generative/Smart.Reflection.Generative.Tools/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Smart.Reflection.Generative.Tools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

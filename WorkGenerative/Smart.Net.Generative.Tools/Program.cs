5ab325d9d33937284a65126c85c5f0c1de6568d0
usausa <machi.pon@gmail.com>
2017-05-20 22:55:44 +0900

    Add projects

---
+++ WorkGenerative/Smart.Net.Generative.Tools/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Smart.Net.Generative.Tools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

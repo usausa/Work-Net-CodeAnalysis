8733d56ad41b06e111b5fbb108d881ce38bb7eae
usausa <machi.pon@gmail.com>
2018-01-01 17:09:02 +0900

    Add files

---
+++ Generative/Smart.Reflection.Generative.CodeGenerator/Reflection/Generative/CodeGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Smart.Reflection.Generative.CodeGenerator
+{
+    using System;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

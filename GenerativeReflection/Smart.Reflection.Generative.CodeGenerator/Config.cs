1f25bb49cd5fdcbd77641afbbd8c6a1f3245789c
usausa <machi.pon@gmail.com>
2018-01-03 17:35:21 +0900

    Update baseline

--- GenerativeReflection/Smart.Reflection.Generative.CodeGenerator/Config.cs
+++ GenerativeReflection/Smart.Reflection.Generative.CodeGenerator/Config.cs
@@ -2,5 +2,10 @@
 {
     public class Config
     {
+        public string[] TargetDirectories { get; set; }
+
+        public string[] ExcludeDirectories { get; set; } = { "bin", "obj" };
+
+        public string FilePattern { get; set; } = "*.cs";
     }
 }

bd08b39386af9ab9815b6f04bc4216283698ce4e
usausa <machi.pon@gmail.com>
2018-06-10 16:40:16 +0900

    Rename project

--- Sandbox/CompileBasicTest/CompileBasicTest/Program.cs
+++ Sandbox/CompileBasicTest/CompileBasicTest/Program.cs
@@ -1,10 +1,9 @@
-﻿using System.Linq;
-
-namespace CompileBasicTest
+﻿namespace CompileBasicTest
 {
     using System;
     using System.IO;
     using System.Reflection;
+    using System.Linq;
 
     using Microsoft.CodeAnalysis;
     using Microsoft.CodeAnalysis.CSharp;
